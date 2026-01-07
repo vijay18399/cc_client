dfce697ce27b9ba86ee8f1bbc26bee6845ce6bde
Medapati Vijay Reddy <vjmedapati@gmail.com>
2026-01-07 09:21:11 +0530

    baseURL updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // This will be proxied to the backend server
+  baseURL: 'https://cc-backend-vsve.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
